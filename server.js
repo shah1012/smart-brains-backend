f84b62f7a196ccb147467959560f5ec80a18d3af
shah1012 <69828183+shah1012@users.noreply.github.com>
2021-03-12 15:53:59 -0600

    Added More Routes

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require("express");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
 const app = express();
+
+app.use(express.json());
+app.use(cors());
 
 const database = {
   users: [
     {
-      id: 123,
+      id: "123",
       name: "John",
       email: "john@gmail.com",
       password: "cookies",
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: "124",
+      name: "Sally",
+      email: "sally@gmail.com",
+      password: "bananas",
       entries: 0,
       joined: new Date(),
     },
@@ -16,12 +29,68 @@ const database = {
 
 // ROUTES
 app.get("/", (req, res) => {
-  res.send("this is working");
+  res.send(database.users);
 });
 
 // SIGNIN ROUTE
 app.post("/signin", (req, res) => {
-  res.json("Signin");
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json(database.users[0]);
+  } else {
+    res.status(400).json("error logging in");
+  }
+});
+
+app.post("/register", (req, res) => {
+  const { name, email, password } = req.body;
+
+  database.users.push({
+    id: "125",
+    name: name,
+    email: email,
+    entries: 0,
+    joined: new Date(),
+  });
+
+  res.json(database.users[database.users.length - 1]);
+});
+
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+
+  let found = false;
+
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  });
+
+  if (!found) {
+    res.status(400).json("Not found");
+  }
+});
+
+app.put("/image", (req, res) => {
+  const { id } = req.body;
+
+  let found = false;
+
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  });
+
+  if (!found) {
+    res.status(400).json("Not found");
+  }
 });
 
 app.listen(3000, () => {
@@ -34,6 +103,4 @@ app.listen(3000, () => {
   /register -> POST = user
   /profile/:userId -> get request
   /image -> PUT -> user
-
-
 */
